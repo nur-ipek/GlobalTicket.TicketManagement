89076d7bbc6cfba4efcc227badd78bf83a82023e
Nur İpek Yılmaz <nur.yilmaz@mbi.com.tr>
2021-04-14 02:32:33 +0300

    Add project files.

---
+++ GlobalTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T:class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T Entitiy);
+        Task<T> UpdateAsync(T Entitiy);
+        Task<T> DeleteAsync(T Entity);
+
+
+    }
+}
---
+++ GlobalTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Domain.Common
+{
+    public class AuditableEntity
+    {
+    }
+}
